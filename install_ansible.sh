da491848eab52bc7f8ef7ee63a2eabf673b5ac9d
Wirkmood <wirkmood@gmail.com>
2017-10-20 09:22:57 +0200

    Delete install_ansible.sh

--- install_ansible.sh
+++
@@ -1,4 +0,0 @@
-apt-get install software-properties-common -y
-echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" >> /etc/sources.list
-apt-get update -y 
-apt-get install ansible -y 
